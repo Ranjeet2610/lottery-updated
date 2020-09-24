13ba30c93eb4c25c27a535ddb70313fa6a5ccb23
Ankit bhadauria <bhadauria.ankitsingh2020@gmail.com>
2020-09-24 15:38:54 +0530

    done ticket UI

--- src/components/lotteryT.js
+++ src/components/lotteryT.js
@@ -10,7 +10,9 @@ export default class Lottery extends Component {
             propsInfo:this.props.info,
             addEvent:[],
             resultDate:'',
-            qrInfo:''
+            qrInfo:'',
+            tgTime:'',
+            tgDate:''
         }
     }
 
@@ -24,7 +26,18 @@ export default class Lottery extends Component {
         .catch(error=>{
             console.log(error)
         })
+        this.date();
         this.filterAmount();
+    }
+
+    date = async() => {
+        let arr = this.state.propsInfo.ele.createdAt.split("T")
+        let t = arr[1].substring(0,5);
+        await this.setState({
+            tgDate:arr[0],
+            tgTime:t
+        })
+        console.log(this.state.tgDate,this.state.tgTime)
     }
 
 
@@ -93,24 +106,22 @@ export default class Lottery extends Component {
                     // <div className="logo"></div>
                     // <!-- First Container -->
                     }
-                    
-                    
-                    <div className="container" >
+        
                         <div className="row row-one" >
-                            <div className="col-lg-3 col-3" >
+                            <div className="col-lg-4 col-5">
                                 <small className="heading-1">{this.state.propsInfo.ele._id}</small>
                             </div>
 
-                            <div className="col-lg-2 col-2 offset-2">
-                                <small className="heading-2" >***RECEIPT***</small>
+                            <div className="col-lg-4 col-2">
+                                <small className="heading-2 offset-0 blah" >***RECEIPT***</small>
                             </div>
                         
 
-                            <div className="col-lg-3  col-5 offset-0">
-                                <small className="heading-3" >{this.state.propsInfo.ele.createdAt}{/*27/08/2020 - 03:30 PM*/}</small>
+                            <div className="col-lg-4  col-5 ">
+                                <small className="heading-3 ml-4 mt-2" >{this.state.tgDate}&nbsp;-&nbsp;{this.state.tgTime} PM</small>
                             </div>
 
-                        </div>
+            
                     </div>
                 
                     {
@@ -142,18 +153,18 @@ export default class Lottery extends Component {
                         // <!-- Thrid Container -->  
                     }
                     
-                    <div className="container text-center mt-4 pl-4">
-                        <div className="row">
-                            <div className="col-lg-2 col-md-2 col-3 nos">
+                    <div className="container text-center mt-4">
+                        <div className="row num ml-1">
+                            <div className="col-lg-2 col-md-2 col-3 nos ">
                                 <small><span style={{fontSize: "22px", fontWeight: "500"}}>1</span>(One)</small> <br/>
                                 <small>₹{this.state.propsInfo.ele.one}</small>
                             </div>
 
-                            <div className="col-lg-2 col-md-2 col-3 nos">
+                            <div className="col-lg-2 col-md-2 col-3 nos ml-1">
                                 <small><span style={{fontSize: "22px", fontWeight: "500"}}>2</span>(Two)</small> <br/>
                                 <small>₹{this.state.propsInfo.ele.two}</small>
                             </div>
-                            <div className="col-lg-2 col-md-3 col-3 nos">
+                            <div className="col-lg-2 col-md-3 col-3 nos ml-1">
                                 <small><span style={{fontSize: "22px", fontWeight: "500"}}>3</span>(Three)</small><br/>
                                 <small>₹{this.state.propsInfo.ele.three}</small>
                             </div>
@@ -161,12 +172,12 @@ export default class Lottery extends Component {
                                 <small><span style={{fontSize: "22px", fontWeight: "500"}}>4</span>(Four)</small> <br/>
                                 <small>₹{this.state.propsInfo.ele.four}</small>
                             </div>
-                            <div className="col-lg-2 col-md-2 col-3 nos">
+                            <div className="col-lg-2 col-md-2 col-3 nos ml-1">
                                 <small><span style={{fontSize: "22px", fontWeight: "500"}}>5</span>(Five)</small> <br/>
                                 <small>₹{this.state.propsInfo.ele.five}</small>
                             </div>
                         
-                            <div className="col-lg-2 col-md-2 col-3 nos">
+                            <div className="col-lg-2 col-md-2 col-3 nos ml-1">
                                 <small><span style={{fontSize: "22px", fontWeight: "500"}}>6</span>(Six)</small> <br/>
                                 <small>₹{this.state.propsInfo.ele.six}</small>
                             </div>
@@ -176,12 +187,12 @@ export default class Lottery extends Component {
                                 <small>₹{this.state.propsInfo.ele.seven}</small>
                             </div>
 
-                            <div className="col-lg-2 col-md-3 col-3 nos">
+                            <div className="col-lg-2 col-md-3 col-3 nos ml-1">
                                 <small><span style={{fontSize: "22px", fontWeight: "500"}}>8</span>(Eight)</small> <br/>
                                 <small>₹{this.state.propsInfo.ele.eight}</small>
                             </div>
 
-                            <div className="col-lg-2 col-md-3 col-3 nos">
+                            <div className="col-lg-2 col-md-3 col-3 nos ml-1">
                                 <small><span style={{fontSize: "22px", fontWeight: "500"}}>9</span>(Nine)</small> <br/>
                                 <small>₹{this.state.propsInfo.ele.nine}</small>
                             </div>
@@ -194,7 +205,7 @@ export default class Lottery extends Component {
                     </div>
                     <div className="row row-six">
                         <div className="col-lg-6 col-6">
-                            <h5>total amount</h5>
+                            <h5 style={{paddingTop:'5px'}}>total amount</h5>
                         </div>
 
                         <div className="col-lg-6 col-6">
