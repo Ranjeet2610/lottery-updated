b15346271daaa14fb6a6be6aaf174360c0732872
Ankit bhadauria <bhadauria.ankitsingh2020@gmail.com>
2020-09-24 20:50:32 +0530

    filtered settlement

--- src/components/Manage/ManageLottery.js
+++ src/components/Manage/ManageLottery.js
@@ -4,15 +4,39 @@ export default class ManageLottery extends Component {
     constructor(props){
         super(props);
         this.state={
-            lotteryData:this.props.lottery,
+            lotteryData:this.props.lottery.Data,
+            drdown:this.props.lottery.Data
+        }
+    }
+
+    handleFilter= async(e) => {
+        // debugger
+        let dataArray = [...this.state.drdown]
+        let searchUser = e.target.value.toLowerCase();
+        const updateList = dataArray.filter(user => user.status===searchUser)
+        if(searchUser!==""){
+            await this.setState({
+                lotteryData:updateList
+            })
+        }
+        else{
+            await this.setState({
+                lotteryData:this.state.drdown
+            })
         }
     }
 
     render() {
-        let i=0;
-        return (
+            return (
                 <div className="section__content border section__content--p30 mt-5">
-                <h1 className="ml-3 mb-4 border">Manage Lottery</h1>
+                    <div className="d-flex row">
+                        <h1 className="ml-3 mb-4 col-lg-4 col-12 border">Manage Lottery</h1>
+                        <select name="drdown" className="ml-5 col-lg-3 col-9 drdn mb-4 w-25" style={{borderRadius:'10px',border:"none",outline:'none  '}} onChange={this.handleFilter}>
+                            <option value="">Open/Settled</option>
+                            <option value="open">Open</option>
+                            <option value="settled">settled</option>
+                        </select>
+                    </div>
                     <div className="container-fluid">
                         <div className="row">
                             <div className="col-lg-12">
@@ -23,27 +47,29 @@ export default class ManageLottery extends Component {
                                                 <th>S.No.</th>
                                                 <th>agent&nbsp;ID</th>
                                                 <th>agent&nbsp;name</th>
+                                                <th>Status</th>
                                                 <th>ticket&nbsp;number</th>
                                                 <th>ticket&nbsp;value</th>
                                             </tr>
                                         </thead>
+                                        <tbody>
+
                                         {
-                                            this.state.lotteryData.Data.map(ele=>{
-                                                i+=1
+                                            this.state.lotteryData.map((ele,i)=>{
                                                 return(
-                                                <tbody>
                                                     <tr>
-                                                        <td className="align-middle">{i}</td>
+                                                        <td className="align-middle">{i+1}</td>
                                                         <td className="align-middle">{ele.agentId}</td>
                                                         <td className="align-middle">{ele.agentName}</td>
+                                                        <td className="align-middle">{ele.status}</td>
                                                         <td className="align-middle">{ele._id}</td>
                                                         <td className="align-middle">{ele.totalAmount}</td>
                                                     </tr>
-                                                </tbody>
                                                 )
-                                            }
-                                                )
-                                            }
+                                            })
+                                        }
+                                        </tbody>
+
                                     </table>
                                 </div>
                             </div>
